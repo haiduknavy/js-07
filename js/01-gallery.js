0fcf94b729df1370cbac303773f835053a2a21a0
haiduknavy <haiduknavy@gmail.com>
2021-10-23 13:59:37 +0300

    end HW 7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,47 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery')
+
+const makeGalleryEl = galleryItems.map(item => {
+    return `
+    <div class="gallery__item">
+  <a class="gallery__link" href='${item.original}'>
+    <img
+      class="gallery__image"
+      src='${item.preview}'
+      data-source='${item.original}'
+      alt='${item.description}'
+    />
+  </a>
+</div>
+    `
+}).join('');
+
+gallery.insertAdjacentHTML('afterbegin',makeGalleryEl)
+
+function onGalleryRefOpen(event){
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  const instance = basicLightbox.create(`
+    <img src=${event.target.dataset.source} width="800" height="600">
+`)
+  
+  instance.show();
+
+  window.addEventListener('keydown', (event) => {
+   
+    if (event.key === 'Escape') {
+      instance.close();
+    }
+  });
+    
+};
+
+gallery.addEventListener('click', onGalleryRefOpen);
+
+
+
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,31 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery')
+
+const makeGalleryEl = galleryItems.map(item => {
+    return `
+    
+  <a class="gallery__item" href='${item.original}'>
+    <img
+      class="gallery__image"
+      src='${item.preview}'
+      data-source='${item.original}'
+      alt='${item.description}'
+    />
+  </a>
+
+    `
+}).join('');
+gallery.insertAdjacentHTML('afterbegin', makeGalleryEl)
+
+function onGalleryRefOpen(event){
+  event.preventDefault();
+  let gallery = new SimpleLightbox('.gallery a',{captions:true,captionSelector:'img',captionsData:"alt",captionDelay:250,animationSpeed:350});
+    gallery.open();
+};
+
+gallery.addEventListener('click', onGalleryRefOpen);
+
+
+
